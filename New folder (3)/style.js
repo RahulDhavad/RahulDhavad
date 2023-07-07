410cbf52a53e13731a0531936eae90f02f7f69db
RahulDhavad <105032768+RahulDhavad@users.noreply.github.com>
2023-07-07 01:57:30 -0700

    Add files via upload

---
+++ New folder (3)/style.js
@@ -0,0 +1,26 @@
+
+/*function display(value){
+    console.log(value);
+    document.getElementById('result').value += value;
+    
+}
+function solve(){
+
+    let x = document.getElementById('result').value;
+
+    let y = eval(x);
+
+    document.getElementById('result').value = y;
+
+    return y
+
+}
+function clearScreen(){
+
+    document.getElementById('result').value = ''
+
+} 
+
+let arr = [122,555,'hhh','ecc'];
+arr.unshift('444');
+console.log(arr);
